208f34d7a3faabd1dcc7d42a23afd76850665647
Mahmud <kmahmud101@gmail.com>
2019-11-18 01:17:05 +0600

    Build Craglist Web app

--- craglist_clone/mainapp/views.py
+++ craglist_clone/mainapp/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
+import requests
+from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from . import models
+BASE_CRAGLIST_URL = 'https://bangladesh.craigslist.org/search/eee?query={}'
 # Create your views here.
 def home(request):
     return render(request,'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    #print(search)
+    final_url = BASE_CRAGLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    text = response.next
+    soup = BeautifulSoup(text, features='html.parser')
+    post_title = soup.findall('a',{'class':'result-title'})
+    post_title = soup.findall('a',{'class':'result-title'})
+    print(post_title)
+
+    stuff_for_fronted ={
+        'search':search
+    }
+    return render(request,'my_app/new_search.html',stuff_for_fronted)
+
+
+
