0aa326f4e060cd053c09ba9e569a4cd4971168e6
Mahmud <kmahmud101@gmail.com>
2019-11-18 01:15:30 +0600

    Build todo Basic app

--- todo/mainapp/views.py
+++ todo/mainapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import TodoItem
 
-# Create your views here.
+def todoView(request):
+    all_todo_items = TodoItem.objects.all()
+    return render (request,"todo.htm", {
+        'all_items':all_todo_items
+    })
+
+def addTodo(request):
+   c = request.POST['content']
+   new_item = TodoItem(content=c)
+   new_item.save()
+   return redirect('/')
+
+def deleteTodo(request,todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return redirect('/')
+
+
+
+
--- todo/todoapp/urls.py
+++ todo/todoapp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp.views import todoView,addTodo,deleteTodo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', todoView),
+    path('addTodo/', addTodo),
+    path('deleteTodo/<int:todo_id>/', deleteTodo),
 ]
